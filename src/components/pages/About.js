b0fd6047904f22cc3fedd24362c2d0bfd5d04db4
Brooks Rosenberg <bjtr@bu.edu>
2022-03-23 20:01:43 -0400

    version 1, text added, images not wroking

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div className='homepage'>
+      <h1>About Me</h1>
+      <p>
+      Hi I'm Brooks! I am a recent college graduate from Boston University and have a bachelor's degree in
+            Economics. I've taken up coding and web development as I see it as a cutting-edge field for the next 25
+            years, and somehow I've figured out how to say 'Hello World' and a few more things along the way.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/Work.js
@@ -0,0 +1,56 @@
+import './work.css'
+import './images'
+import React from "react";
+
+export default function Work() {
+  return (
+    <div>
+      <article className="my-work">
+        <h2>My Work</h2>
+        <p className="assignment1">
+          <figure>
+            <a href="https://desolate-springs-51329.herokuapp.com">
+              <img
+                src = './images/carEzScreenshot.png'
+                alt="CarEz"
+            
+              ></img>
+            </a>
+          </figure>{" "}
+          CarEz: This fullstack application is a website that allows users to
+          create an account and interact on a car forum by making posts for a
+          variety of car models
+        </p>
+
+        <p className="assignment2">
+          <figure>
+            <a href="https://brooksrosenberg.github.io/dayPlanner/">
+              <img
+                src="./images/dayplannerscreeenshot.png"
+                alt="Day Planner"
+      
+              ></img>
+            </a>
+          </figure>{" "}
+          Day Planner: This application is a daily scheduling website that lets
+          users build,change and save a daily planner for the work day
+        </p>
+
+        <p className="assignment3">
+          <figure>
+            <a href="https://brooksrosenberg.github.io/theOracleProject/">
+              <img
+                src="./assets/images/OracleProjectScreenshot.png"
+                alt="The Oracle Project"
+              
+              ></img>
+            </a>
+          </figure>{" "}
+          The Oracle Project: This application is a website that tells users
+          their daily horoscope based off their star sign and a random fact of
+          the day
+        </p>
+      </article>
+    </div>
+  );
+}
